c3465a635f464e4f1d62c09ea6e1f25ec063041c
jenningsje <53481699+jenningsje@users.noreply.github.com>
2023-05-22 13:03:23 -0500

    Create config-gatk-pipeline.sh
    Configuration script for the variant-calling pipeline

---
+++ config-gatk-pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+singularity pull --arch amd64 library://goita/collection/gatk.sif:latest
+
+singularity build --remote gatk.sif-latest.sh gatk.def
+
+chmod u+x ./haplotype.sh
+chmod u+x ./bulk-process.sh
+chmod u+x ./GenotypeGVCFs.sh
